67c1dda5722332ece4bf2658c54405b4277707d1
dorothee <dorotheemballa28@gmail.com>
2021-02-20 20:22:42 +0100

    envoi d'Email & formulaire de contact cKEDITOR

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use App\Repository\AnnoncesRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -26,5 +30,37 @@ class MainController extends AbstractController
     public function mentions(): Response
     {
         return $this->render('main/mentions.html.twig');
+    }
+     /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(Request $request, MailerInterface $mailer)
+    {
+        // generer form
+        $form = $this->createForm(ContactType::class);
+
+        $contact = $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $email = (new TemplatedEmail())
+                ->from($contact->get('email')->getData())
+                ->to('Vous@domaine.fr')
+                ->subject('contact depuis le site PetitesAnnonces')
+                ->htmlTemplate('emails/contact.html.twig')
+                ->context([
+                    'mail' => $contact->get('email')->getData(),
+                    'sujet' => $contact->get('sujet')->getData(),
+                    'message' => $contact->get('message')->getData(),
+                ])
+                ;
+                $mailer->send($email);
+                $this->addFlash('message', 'Mail envoyé');
+                return $this->redirectToRoute('contact');
+        }
+
+        return $this->render('main/contact.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
